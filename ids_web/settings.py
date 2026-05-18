c1a72bb0217edadad43a68c22a4e0fa54c625e03
Cristian Moscoso <cmoscoso@inacap.cl>
2026-05-18 15:01:32 -0400

    feat: migrar a PostgreSQL persistente

--- ids_web/settings.py
+++ ids_web/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import resend
+import dj_database_url
 
 # BASE
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +74,10 @@ WSGI_APPLICATION = "ids_web.wsgi.application"
 
 # BASE DE DATOS
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+    )
 }
 
 # PASSWORDS
